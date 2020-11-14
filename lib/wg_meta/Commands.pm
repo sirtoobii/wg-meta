459ad5f151c7884954a090e915708b11b04c4ec2
Tobias Bossert <tobias.bossert@fastpath.ch>
2020-11-14 10:26:44 +0100

    added wg show parser

---
+++ lib/wg_meta/Commands.pm
@@ -0,0 +1,14 @@
+package wg_meta::Commands;
+use strict;
+use warnings FATAL => 'all';
+
+use experimental 'signatures';
+use base 'Exporter';
+our @EXPORT = qw(command_show);
+
+
+sub command_show($ref_attr_list, $ref_wg_meta_attr_list, $ref_parsed_config, $ref_parsed_show){
+
+}
+
+1;
